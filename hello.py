50525041b8d7769df77f16e2d7f7f657517c64aa
Saiyedul-007 <sayeedul.mursalin07@gmail.com>
2026-09-07 13:37:38 +0300

    Made a program to check whether its positive or negative

--- hello.py
+++ hello.py
@@ -1,6 +1,15 @@
-marks = int(input("Enter the marks of the student: "))
-attendance = int(input("Enter the attendance of the student: "))
-if marks >= 40 and attendance >= 75:
-    print("The student has passed the exam.")
+# marks = int(input("Enter the marks of the student: "))
+# attendance = int(input("Enter the attendance of the student: "))
+# if marks >= 40 and attendance >= 75:
+#     print("The student has passed the exam.")
+# else:
+#     print("The student has failed.")
+
+# WAP to check whether a number is positive, negative or zero
+input_number = float(input("Enter a number:"))
+if input_number > 0:
+    print(f"The number {input_number} is positive.")
+elif input_number < 0:
+    print(f"The number {input_number} is negative.")
 else:
-    print("The student has failed.")
+    print(f"The number {input_number} is zero.")
