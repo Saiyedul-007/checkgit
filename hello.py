f5bb48ded4b5c2280c332f9b0c445989c2cc8453
Saiyedul-007 <sayeedul.mursalin07@gmail.com>
2026-09-07 13:41:01 +0300

    Program to check whether the number is even or odd

--- hello.py
+++ hello.py
@@ -5,11 +5,18 @@
 # else:
 #     print("The student has failed.")
 
-# WAP to check whether a number is positive, negative or zero
-input_number = float(input("Enter a number:"))
-if input_number > 0:
-    print(f"The number {input_number} is positive.")
-elif input_number < 0:
-    print(f"The number {input_number} is negative.")
-else:
-    print(f"The number {input_number} is zero.")
+# # WAP to check whether a number is positive, negative or zero
+# input_number = float(input("Enter a number:"))
+# if input_number > 0:
+#     print(f"The number {input_number} is positive.")
+# elif input_number < 0:
+#     print(f"The number {input_number} is negative.")
+# else:
+#     print(f"The number {input_number} is zero.")
+
+# WAP to check and display that the number is even or odd
+number = int(input("Enter the number: "))
+if number % 2 == 0:
+    print(f"The number {number} is even.")
+else: 
+    print(f"The number {number} is odd.")
